858ec3b600dd7a1a4263cdf0374746d3d53df638
Sean Wilson <seancwilson@gmail.com>
2018-05-31 11:18:20 -0500

    Better type definition for Sugar's `Object.map`

--- src/object.ts
+++ src/object.ts
@@ -8,7 +8,8 @@ declare global {
 		// better/updated definitions for Sugar's added object methods
 		clone<T extends Object>(instance: T, deep?: boolean): T
 		forEach<T extends Object>(instance: T, fn: (val: T[keyof T], key: keyof T, obj: T) => void): T
-		select<T extends Object>(instance: T, find: string|RegExp|Array<string>|Object): Partial<T>
+		select<T extends Object>(instance: T, find: string | RegExp | Array<string> | Object): Partial<T>
+		map<T extends Object, U>(instance: T, map: keyof T | Object.MapFn<T, U>): Record<keyof T, U>
 
 		mapKeys<T extends Object>(instance: T, map: Object.KeyMap<T>, skipNull?: boolean) : Object
 		cordon<T extends Object>(instance: T, deep?: boolean): Readonly<T>
@@ -17,12 +18,14 @@ declare global {
 		selectValues<T extends Object, K extends keyof T>(instance: T, keys: UnensuredArray<K>): T[K][]
 		getWithDefault<T extends Object, U>(instance: T, key: string, dfault: U, inherited?: boolean): U
 		duplicate<T extends Object>(instance: T, duplicateFn?: Object.DuplicateFn): T
+		when<T>(instance: T, condition: boolean | ((obj: T) => boolean), whenFn: (obj: T) => T): T
 
 		isDefined(instance: any): boolean
 	}
 
 	namespace Object {
-		type CollectFn<T extends Object, U> = (val: T[keyof T], key: keyof T, obj: T) => U;
+		type CollectFn<T extends Object, U> = (val: T[keyof T], key: keyof T, obj: T) => U
+		type MapFn<T extends Object, U> = CollectFn<T, U>
 		type KeyMap<T extends Object> = ((key: keyof T, value: T[keyof T], obj: T) => primitive) | { [key: string]: primitive }
 		type DuplicateFn = <T extends Object>(orig: T) => T | typeof Sugar
 	}
@@ -83,7 +86,7 @@ Sugar.Object.defineInstanceAndStatic({
 			else if (Object.isArray(instance))
 				result = <any>instance.map(item => Object.duplicate(item, duplicateFn))
 			else if (Object.isObject(instance))
-				result = <any>Object.map(instance, val => <any>Object.duplicate(val, duplicateFn))
+				result = <any>Object.map(instance, val => <any>Object.duplicate(<any>val, duplicateFn))
 			else
 				result = Object.clone(instance)
 		}
