75ac0ce2943f21fb11fabaf1aee471c89f36edd3
Sean Wilson <seancwilson@gmail.com>
2019-07-05 11:35:45 -0500

    Add `Object.assign` polyfill

--- src/object.ts
+++ src/object.ts
@@ -3,6 +3,43 @@ import { UnensuredArray } from './array'
 
 declare global {
 	interface ObjectConstructor {
+		// #region polyfills
+		/**
+		 * Copy the values of all of the enumerable own properties from one or more source objects to a
+		 * target object. Returns the target object.
+		 * @param target The target object to copy to.
+		 * @param source The source object from which to copy properties.
+		 */
+		assign<T, U>(target: T, source: U): T & U
+
+		/**
+		 * Copy the values of all of the enumerable own properties from one or more source objects to a
+		 * target object. Returns the target object.
+		 * @param target The target object to copy to.
+		 * @param source1 The first source object from which to copy properties.
+		 * @param source2 The second source object from which to copy properties.
+		 */
+		assign<T, U, V>(target: T, source1: U, source2: V): T & U & V
+
+		/**
+		 * Copy the values of all of the enumerable own properties from one or more source objects to a
+		 * target object. Returns the target object.
+		 * @param target The target object to copy to.
+		 * @param source1 The first source object from which to copy properties.
+		 * @param source2 The second source object from which to copy properties.
+		 * @param source3 The third source object from which to copy properties.
+		 */
+		assign<T, U, V, W>(target: T, source1: U, source2: V, source3: W): T & U & V & W
+
+		/**
+		 * Copy the values of all of the enumerable own properties from one or more source objects to a
+		 * target object. Returns the target object.
+		 * @param target The target object to copy to.
+		 * @param sources One or more source objects from which to copy properties
+		 */
+		assign(target: object, ...sources: any[]): any
+		// #endregion
+
 		// better/updated definitions for Sugar's added object methods
 		clone<T extends Object>(instance: T, deep?: boolean): T
 		forEach<T extends Object>(instance: T, fn: (val: T[keyof T], key: keyof T, obj: T) => void): T
@@ -130,5 +167,11 @@ Sugar.Object.defineInstanceAndStatic({
 Sugar.Object.defineStatic({
 	isDefined(instance: any) {
 		return (typeof instance !== 'undefined')
+	}
+})
+
+Sugar.Object.defineStaticPolyfill({
+	assign(target: Object, sources: Object[]) {
+		return Object.mergeAll(target, sources)
 	}
 })
